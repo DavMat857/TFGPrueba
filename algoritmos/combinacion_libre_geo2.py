aacbe14f0fd7d4af9d2e2473b7f0c545ada9d468
DavMat857 <124212989+DavMat857@users.noreply.github.com>
2023-06-14 18:39:16 +0200

    Update combinacion_libre_geo2.py

--- algoritmos/combinacion_libre_geo2.py
+++ algoritmos/combinacion_libre_geo2.py
@@ -2,6 +2,7 @@
 from funcionesdef import*
 import numpy as np
 
+#Método 1 tuve que cambiar el umbral porque duplicada la cantidad de saltos
 
 #Datos a utilizar
 filename = "datos/MAD1047A00.23O"
@@ -18,8 +19,8 @@ def visualizacion():
     
 
 #Algoritmo
-
-def algoritmo(datos = geo,numero_muestras=numero_muestras,multiplo=3, tiempo = 1):
+#Umbral media + sigma
+def algoritmo(datos = geo,numero_muestras=numero_muestras,multiplo=1, tiempo = 1):
      
     graf_datos(datos, "Algoritmo_geometria", tiempo)
     resultados = alg_sacar_saltos(datos,numero_muestras,multiplo)
@@ -38,10 +39,10 @@ def alg_sacar_saltos(datos,numero_muestras,multiplo):
     valores = list(datos.values())
     #Primer polinomio
     pol, umbral= crear_pol(claves[0:numero_muestras],valores[0:numero_muestras])
-    while i<len(datos)-10 :
+    while i<len(datos) - numero_muestras :
     #for i in range(0,len(datos),numero_muestras):
         
-        if claves[i]- claves[i] > 30:
+        if claves[i]- claves[i-1] > 30:
           saltos.append(claves[i])
           i=i+1
           pol, umbral= crear_pol(claves[i:i+numero_muestras],valores[i:i+numero_muestras])
@@ -50,20 +51,20 @@ def alg_sacar_saltos(datos,numero_muestras,multiplo):
             valor_real = valores[i]
             valor_pol = pol(claves[i])
             error = np.abs(valor_real - valor_pol)
-
+            
             
             if error  > multiplo*umbral:
                 saltos.append(claves[i])
-                i=i+1
+                i=i+numero_muestras
                 pol, umbral= crear_pol(claves[i:i+numero_muestras],valores[i:i+numero_muestras])
         
             else:
-                saltos.append(0)
-                i=i+1
-                     
+                
+                i=i+1            
     return np.array(saltos)
 
 #Creación de un polinomio
+    
 def crear_pol(claves,valores):
     
     degree = 2
@@ -72,9 +73,11 @@ def crear_pol(claves,valores):
     p  = np.poly1d(coeffs)
     pol = [p(n) for n in claves]
     
+    error = [abs(valores[i]-pol[i]) for i in range(len(valores))]
     valor_real = np.array(valores)
     valor_pol = np.array(pol)
     error = np.abs(valor_real - valor_pol)
-    umbral = np.mean(error) + np.std(error)
-    
+    umbral = np.mean(error) + 2*np.std(error)
+    #umbral = np.std(error)
+  
     return p,umbral
