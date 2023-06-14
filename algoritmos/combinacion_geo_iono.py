0c8a4b4c40dd6dd890edc09e32a644995ae16747
DavMat857 <124212989+DavMat857@users.noreply.github.com>
2023-06-14 18:52:48 +0200

    Update combinacion_geo_iono.py

--- algoritmos/combinacion_geo_iono.py
+++ algoritmos/combinacion_geo_iono.py
@@ -19,25 +19,23 @@ def algoritmo(io=io,geo=geo,numero_muestras=numero_muestras, tiempo = 1) :# umbr
     saltos_comunes = []
     duda_geo = []
     duda_ion = []
-    umbral_geo = selector_umbral(geo,numero_muestras)[0]+selector_umbral(geo,numero_muestras)[1]
-    umbral_io = selector_umbral(io,numero_muestras)[0]+ selector_umbral(io,numero_muestras)[1]
-   #Obtención de saltos de ciclo para las combinaciones mencionadas y % datos en común
+#    umbral_geo = selector_umbral(geo,numero_muestras)[0]+selector_umbral(geo,numero_muestras)[1]
+#    umbral_io = selector_umbral(io,numero_muestras)[0]+ selector_umbral(io,numero_muestras)[1]
+    
+    umbral_geo = 3*selector_umbral(geo,numero_muestras)[1]
+    umbral_io = 3*selector_umbral(io,numero_muestras)[1]
+#   #Obtención de saltos de ciclo para las combinaciones mencionadas y % datos en común
     saltos_geo = alg_sacar_saltos(f1menosf2(l1,l2),numero_muestras,umbral_geo)
     saltos_ion = alg_sacar_saltos(io,numero_muestras,umbral_io)
-    comp = saltos_geo == saltos_ion
+    #Comunes
+    comp = [saltos_geo[i] for i in range(min(len(saltos_geo),len(saltos_ion))) if abs(saltos_geo[i]-saltos_geo[i])<10]
     print(f"Coinciden en el {(list(comp).count(True)/len(comp))*100}% de los datos")
-    
-    for i in range(len(saltos_geo)):
-        if saltos_geo[i] != saltos_ion[i] :
-            if saltos_geo[i]!= 0:
-                duda_geo.append(saltos_geo[i])
-           # print(f"Hay que revisar que sucede entre {geo[i]} y {ion[i]}") # Este print puede ser útil
-            else:
-                duda_ion.append(saltos_ion[i])
-        elif saltos_geo[i] == saltos_ion[i] and saltos_geo[i] !=0:
-            saltos_comunes.append(i)
+    comp = [i for i in comp if i !=0]
+    duda_geo= set(saltos_geo) - set(comp)
+    duda_ion= set(saltos_ion) - set(comp)
+
             
-    print(f"Podemos afirmar con mayor seguridad que hay salto de ciclo en las observaciones {saltos_comunes}")
+    print(f"Podemos afirmar con mayor seguridad que hay salto de ciclo en las observaciones {comp}")
     print(f"Hay dudas de que haya salto de ciclo en geo para los instantes {duda_geo} y en ion para {duda_ion}")     
 
     duda_geo = list(map(lambda x: x*tiempo,duda_geo))
